b4e3c79b4308d1326011e883d036c633dadbf3e8
Musangamfura Emmanuel <musangamfure@gmail.com>
2023-07-27 07:56:13 +0100

    Update the files.

--- person.rb
+++ person.rb
@@ -23,4 +23,9 @@ class Person < Nameable
   def correct_name
     @name
   end
+  
+    def add_rental(rental)
+    @rentals << rental
+    rental.person = self
+  end
 end
--- student.rb
+++ student.rb
@@ -1,13 +1,16 @@
 require_relative 'person'
+
 class Student < Person
   attr_reader :classroom
 
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
+    classroom.add_student(self) unless classroom.nil? 
   end
 
   def play_hooky
     '¯\(ツ)/¯'
   end
 end
+
