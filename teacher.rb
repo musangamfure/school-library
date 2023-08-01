20c0303d9b7d898b8a3d2f6bfda6474dd289342f
lordksix <121689749+lordksix@users.noreply.github.com>
2023-08-01 18:50:30 -0500

    Implement to_json and to_hash methods

--- teacher.rb
+++ teacher.rb
@@ -1,13 +1,29 @@
 require_relative 'person'
+
 class Teacher < Person
   attr_reader :specialization
 
   def initialize(age, specialization, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission: parent_permission)
+    super(age, 'Teacher', name, parent_permission:)
     @specialization = specialization
   end
 
   def can_use_services?
     true
+  end
+
+  def to_hash
+    {
+      id: id.to_s,
+      title: 'Teacher',
+      name: @name,
+      age: @age,
+      specialization: @specialization,
+      parent_permission: @parent_permission
+    }
+  end
+
+  def to_json(*_args)
+    to_hash.to_json
   end
 end
